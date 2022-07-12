eb146386c567fef3a89ff449864645df358cb80c
aula <martinconcetti97@gmail.com>
2022-07-12 11:36:36 -0300

    Empty player and ball movement scrips

--- Recuperatorio_Concetti/Assets/Player.cs
+++ Recuperatorio_Concetti/Assets/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+    void CreateBalls()
+    {
+
     }
 }
